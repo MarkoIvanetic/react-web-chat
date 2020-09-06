995ea9581222b7687af56dfc95cc71a5472204c7
marko.ivanetic <marko.ivanetic@cloudsensesolutions.com>
2020-09-06 19:17:00 +0200

    Base functionallity covered

---
+++ src/components/Header.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { auth } from '../services/firebase';
+import { logout } from '../helpers/auth';
+import logo from '../we-chat-logo.png';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <Link className="navbar-logo" to={auth().currentUser ? "/chat" : "/"}>
+          <img src={logo} alt="Logo" />
+          Web Chat
+        </Link>
+        <div className="navbar-user" id="navbarNavAltMarkup">
+          {auth().currentUser ? (
+            <div className="navbar-nav">
+              <Link className="nav-item nav-link mr-3" to="/chat">
+                Profile
+              </Link>
+              <a
+                href=""
+                onClick={e => {
+                  e.preventDefault();
+                  logout();
+                }}
+              >
+                Logout
+              </a>
+            </div>
+          ) : (
+            <div className="navbar-nav">
+              <Link className="nav-item nav-link mr-3" to="/login">
+                Sign In
+              </Link>
+              <Link className="nav-item nav-link mr-3" to="/signup">
+                Sign Up
+              </Link>
+            </div>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+}
+export default Header;
---
+++ src/pages/Home.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import { Link } from 'react-router-dom';
+import { auth } from '../services/firebase';
+
+export default class HomePage extends Component {
+  render() {
+    return (
+      <div className="home">
+        <Header></Header>
+        <section>
+          <div className="jumbotron jumbotron-fluid py-5">
+            <div className="container text-center py-5">
+              <h1 className="display-4">Welcome to Web Chat</h1>
+              <p className="lead">A great place to share your thoughts with friends</p>
+              {auth().currentUser ? null : (
+                <div className="mt-4">
+                  <Link className="btn btn-primary px-5 mr-3" to="/signup">
+                    Create New Account
+                  </Link>
+                  <Link className="btn px-5" to="/login">
+                    Login to Your Account
+                  </Link>
+                </div>
+              )}
+            </div>
+          </div>
+        </section>
+        <Footer></Footer>
+      </div>
+    );
+  }
+}
